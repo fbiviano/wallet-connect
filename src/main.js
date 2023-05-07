5df209a880cd7b7ae3d75464f0c8a06fea4ffde7
disilusionment <121582124+disilusionment@users.noreply.github.com>
2023-05-07 20:09:20 +0200

    added wallet connect

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
+import { configureChains, createClient } from '@wagmi/core'
+import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/html'
 
-console.log('Welcome to Vite + JS + Webflow!')
+const chains = [arbitrum, mainnet, polygon]
+const projectId = '5c9d7874a3cdfb27438c20d2aff57116'
 
-createBadge()
-animateTitle()
+const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  provider,
+})
+const ethereumClient = new EthereumClient(wagmiClient, chains)
+const web3modal = new Web3Modal({ projectId }, ethereumClient)
+
+console.log(web3modal)
